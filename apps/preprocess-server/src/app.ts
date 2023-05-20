408d82c77b24313f32aab42b043cf0b17284f446
Luichix <luisreynaldo.pch@gmail.com>
2023-05-19 22:37:46 -0600

    feat: add Time Preprocessor Microservice

---
+++ apps/preprocess-server/src/app.ts
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+import { Request, Response } from 'express';
+import routerPreprocess from './routes/preprocess';
+
+const app = express();
+const port = process.env.PORT || 3002;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api/setting', routerPreprocess);
+
+app.get('/', (_: Request, res: Response) => {
+  res.send('Time Preprocessor Micro Service');
+});
+
+app.listen(port, () => {
+  console.log(`API Time Preprocessor - Started on port ${port} 🚀`);
+});
---
+++ apps/preprocess-server/src/routes/preprocess.ts
@@ -0,0 +1,5 @@
+import { Router, Request, Response } from 'express';
+
+const routerPreprocess: Router = Router();
+
+export default routerPreprocess;
